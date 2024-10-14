04ad99b438f532c1b5c12a0df41f2d7b45dd1faf
Rupom <mohrajulrupom@gmail.com>
2024-10-14 13:12:56 +0600

    add variable product add update product

--- index.php
+++ index.php
@@ -28,75 +28,138 @@ function wcss_sync_product_to_site($post_id, $post) {
         return;
     }
     $product = wc_get_product($post_id);
-    $site3_url = 'http://localhost/site3/';
-    $site3_client_key = 'ck_6b88b021a658962a376a0403e192afa329e0fe34';
-    $site3_client_secret = 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f';
-    $woocommerce_site = get_woocommerce_client($site3_url, $site3_client_key, $site3_client_secret);
-    $categories = [];
-    foreach ($product->get_category_ids() as $category_id) {
-        $categories[] = ['id' => $category_id];
-    }
-    $data = [
-        'name' => $product->get_name(),
-        'type' => $product->get_type(),
-        'regular_price' => $product->get_regular_price(),
-        'description' => $product->get_description(),
-        'short_description' => $product->get_short_description(),
-        'categories' => $categories,
+    $all_site_info = [
+        [
+            "site_url" => 'http://localhost/site3/',
+            "site_client_key" => 'ck_6b88b021a658962a376a0403e192afa329e0fe34',
+            "site_client_secret" => 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f',
+        ],
+        // [
+        //     "site_url" => 'http://localhost/site3/',
+        //     "site_client_key" => 'ck_6b88b021a658962a376a0403e192afa329e0fe34',
+        //     "site_client_secret" => 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f',
+        // ],
+        // [
+        //     "site_url" => 'http://localhost/site3/',
+        //     "site_client_key" => 'ck_6b88b021a658962a376a0403e192afa329e0fe34',
+        //     "site_client_secret" => 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f',
+        // ]
     ];
-    $woocommerce_site->post('products', $data);
+    foreach($all_site_info as $a_site_info){
+        $woocommerce_site = get_woocommerce_client($a_site_info['site_url'], $a_site_info['site_client_key'], $a_site_info['site_client_secret']);
+        $categories = [];
+        foreach ($product->get_category_ids() as $category_id) {
+            $categories[] = ['id' => $category_id];
+        }
+        $data = [
+            'name' => $product->get_name(),
+            'type' => $product->get_type(),
+            'regular_price' => $product->get_regular_price(),
+            'description' => $product->get_description(),
+            'short_description' => $product->get_short_description(),
+            'categories' => $categories,
+            'sku' => $product->get_sku(),
+        ];
+        $existing_products = $woocommerce_site->get('products', ['sku' => $product->get_sku()]);
+        if (!empty($existing_products)) {
+            $existing_product_id = $existing_products[0]->id;
+            $woocommerce_site->put('products/' . $existing_product_id, $data);
+        } else {
+            if ($product->is_type('variable')) {
+                $new_product = $woocommerce_site->post('products', $data);
+                $new_product_id = $new_product->id;
+                $variations = $product->get_children();
+                foreach ($variations as $variation_id) {
+                    $variation = new WC_Product_Variation($variation_id);
+                    $variation_attributes = [];
+                    foreach ($variation->get_attributes() as $attribute_name => $attribute_value) {
+                        $variation_attributes[] = [
+                            'name' => wc_attribute_label($attribute_name),
+                            'option' => $attribute_value 
+                        ];
+                    }
+                    $variation_data = [
+                        'regular_price' => $variation->get_regular_price(),
+                        'sale_price' => $variation->get_sale_price(),
+                        'sku' => $variation->get_sku(),
+                        'stock_quantity' => $variation->get_stock_quantity(),
+                        'attributes' => $variation_attributes, 
+                    ];
+                    $woocommerce_site->post('products/' . $new_product_id . '/variations', $variation_data);
+                }
+            } else {
+                $woocommerce_site->post('products', $data);
+            }
+        }
+    }
 }
 add_action('woocommerce_thankyou', 'wcss_sync_order_to_site', 10, 1);
 function wcss_sync_order_to_site($order_id) {
     $order = wc_get_order($order_id);
-    $site3_url = 'http://localhost/site3/';
-    $site3_client_key = 'ck_6b88b021a658962a376a0403e192afa329e0fe34';
-    $site3_client_secret = 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f';
-    $woocommerce_site = get_woocommerce_client($site3_url, $site3_client_key, $site3_client_secret);
-    $order_data = [
-        'payment_method' => $order->get_payment_method(),
-        'payment_method_title' => $order->get_payment_method_title(),
-        'set_paid' => $order->is_paid(),
-        'billing' => [
-            'first_name' => $order->get_billing_first_name(),
-            'last_name' => $order->get_billing_last_name(),
-            'address_1' => $order->get_billing_address_1(),
-            'address_2' => $order->get_billing_address_2(),
-            'city' => $order->get_billing_city(),
-            'state' => $order->get_billing_state(),
-            'postcode' => $order->get_billing_postcode(),
-            'country' => $order->get_billing_country(),
-            'email' => $order->get_billing_email(),
-            'phone' => $order->get_billing_phone(),
+    $all_site = [
+        [
+            "site_url" => 'http://localhost/site3/',
+            "site_client_key" => 'ck_6b88b021a658962a376a0403e192afa329e0fe34',
+            "site_client_secret" => 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f',
         ],
-        'shipping' => [
-            'first_name' => $order->get_shipping_first_name(),
-            'last_name' => $order->get_shipping_last_name(),
-            'address_1' => $order->get_shipping_address_1(),
-            'address_2' => $order->get_shipping_address_2(),
-            'city' => $order->get_shipping_city(),
-            'state' => $order->get_shipping_state(),
-            'postcode' => $order->get_shipping_postcode(),
-            'country' => $order->get_shipping_country(),
-        ],
-        'line_items' => [],
-        'shipping_lines' => [],
+        // [
+        //     "site_url" => 'http://localhost/site3/',
+        //     "site_client_key" => 'ck_6b88b021a658962a376a0403e192afa329e0fe34',
+        //     "site_client_secret" => 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f',
+        // ],
+        // [
+        //     "site_url" => 'http://localhost/site3/',
+        //     "site_client_key" => 'ck_6b88b021a658962a376a0403e192afa329e0fe34',
+        //     "site_client_secret" => 'cs_057c576f66bfbdc1dac2762d7ee5d2645affc78f',
+        // ]
     ];
-    foreach ($order->get_items() as $item_id => $item) {
-        $product = $item->get_product();
-        $order_data['line_items'][] = [
-            'product_id' => $product->get_id(),
-            'quantity' => $item->get_quantity(),
-            'subtotal' => $item->get_subtotal(),
-            'total' => $item->get_total(),
+    foreach($all_site as $a_site){
+        $woocommerce_site = get_woocommerce_client($a_site['site_url'], $a_site['site_client_key'], $a_site['site_client_secret']);
+        $order_data = [
+            'payment_method' => $order->get_payment_method(),
+            'payment_method_title' => $order->get_payment_method_title(),
+            'set_paid' => $order->is_paid(),
+            'billing' => [
+                'first_name' => $order->get_billing_first_name(),
+                'last_name' => $order->get_billing_last_name(),
+                'address_1' => $order->get_billing_address_1(),
+                'address_2' => $order->get_billing_address_2(),
+                'city' => $order->get_billing_city(),
+                'state' => $order->get_billing_state(),
+                'postcode' => $order->get_billing_postcode(),
+                'country' => $order->get_billing_country(),
+                'email' => $order->get_billing_email(),
+                'phone' => $order->get_billing_phone(),
+            ],
+            'shipping' => [
+                'first_name' => $order->get_shipping_first_name(),
+                'last_name' => $order->get_shipping_last_name(),
+                'address_1' => $order->get_shipping_address_1(),
+                'address_2' => $order->get_shipping_address_2(),
+                'city' => $order->get_shipping_city(),
+                'state' => $order->get_shipping_state(),
+                'postcode' => $order->get_shipping_postcode(),
+                'country' => $order->get_shipping_country(),
+            ],
+            'line_items' => [],
+            'shipping_lines' => [],
         ];
+        foreach ($order->get_items() as $item_id => $item) {
+            $product = $item->get_product();
+            $order_data['line_items'][] = [
+                'product_id' => $product->get_id(),
+                'quantity' => $item->get_quantity(),
+                'subtotal' => $item->get_subtotal(),
+                'total' => $item->get_total(),
+            ];
+        }
+        foreach ($order->get_shipping_methods() as $shipping_item_id => $shipping_item) {
+            $order_data['shipping_lines'][] = [
+                'method_id' => $shipping_item->get_method_id(),
+                'method_title' => $shipping_item->get_method_title(),
+                'total' => $shipping_item->get_total(),
+            ];
+        }
+        $woocommerce_site->post('orders', $order_data);
     }
-    foreach ($order->get_shipping_methods() as $shipping_item_id => $shipping_item) {
-        $order_data['shipping_lines'][] = [
-            'method_id' => $shipping_item->get_method_id(),
-            'method_title' => $shipping_item->get_method_title(),
-            'total' => $shipping_item->get_total(),
-        ];
-    }
-    $woocommerce_site->post('orders', $order_data);
 }
